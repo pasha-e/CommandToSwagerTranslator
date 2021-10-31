55d279865a54af7dc1590281bb14418b5a3eb185
Pavel Egorov <egorov.p@gmail.com>
2021-10-31 18:36:11 +0300

    initial

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Projects/CommandToSwagerTranslator
+# Build directory: C:/Projects/CommandToSwagerTranslator/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
